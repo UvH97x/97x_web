939ac0586f3c87ae749cc006e595fde51fd93347
UvH97x <uvh.97x@gmail.com>
2024-12-01 23:40:18 +0900

    update article rtenderer test data

--- src/app/applications/article-renderer-test/page.tsx
+++ src/app/applications/article-renderer-test/page.tsx
@@ -5,13 +5,12 @@ import Article from "@/src/components/article_renderer/Lv0/Article";
 
 const ArticleTestPage = () => {
   // サーバー側で直接データを読み込む
-  const filePath = path.join(process.cwd(), "src", "app", "applications", "article-renderer-test", "Article.json");
+  const filePath = path.join(process.cwd(), "src", "app", "applications", "article-renderer-test", "Article2.json");
   const jsonData = fs.readFileSync(filePath, "utf-8");
   const articleData = JSON.parse(jsonData);
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">Article Renderer Test</h1>
+    <div className="container mx-auto py-8">
       <Article content={articleData.content} children={articleData.children} />
     </div>
   );
--- src/components/article_renderer/Lv4/Text.tsx
+++ src/components/article_renderer/Lv4/Text.tsx
@@ -3,13 +3,23 @@
 const TextBlock: React.FC<{ content: any }> = ({ content }) => {
   const { content: text, style } = content;
 
+  // `style`がundefinedの場合にデフォルト値を設定
+  const effectiveStyle = {
+    bold: false,
+    italic: false,
+    underline: false,
+    strikethrough: false,
+    color: "black",
+    ...style, // `style`が定義されていれば上書きする
+  };
+
   return (
     <span
-      className={`${style.bold ? "font-bold" : ""} 
-                  ${style.italic ? "italic" : ""} 
-                  ${style.underline ? "underline" : ""} 
-                  ${style.strikethrough ? "line-through" : ""}`}
-      style={{ color: style.color }}
+      className={`${effectiveStyle.bold ? "font-bold" : ""} 
+                  ${effectiveStyle.italic ? "italic" : ""} 
+                  ${effectiveStyle.underline ? "underline" : ""} 
+                  ${effectiveStyle.strikethrough ? "line-through" : ""}`}
+      style={{ color: effectiveStyle.color }}
     >
       {text}
     </span>
