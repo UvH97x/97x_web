8c6a2fe71978e0a36930f0a073b4e0c991f21e39
UvH97x <uvh.97x@gmail.com>
2026-06-05 06:29:55 +0900

    remove prismjs prism-react-renderer

--- src/app/(article_like)/articles/[genre]/[slug]/_components/MarkdownRenderer.tsx
+++ src/app/(article_like)/articles/[genre]/[slug]/_components/MarkdownRenderer.tsx
@@ -2,45 +2,32 @@
 
 'use client';
 
-import { useEffect } from 'react';
 import Markdown from "react-markdown";
-
-import Prism from 'prismjs';
-import 'prismjs/components/prism-bash';
-import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-jsx';
-import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-tsx';
-import 'prismjs/themes/prism-tomorrow.css';
-import remarkMath from 'remark-math'; // 数式のサポート
-import rehypeKatex from 'rehype-katex'; // KaTeXのサポート
-import 'katex/dist/katex.min.css'; // KaTeXのCSSをインポート
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import rehypeHighlight from 'rehype-highlight';
+import 'katex/dist/katex.min.css';
+import 'highlight.js/styles/tokyo-night-dark.css';
 
 interface MarkdownRendererProps {
-  markdownContent: string; // markdownファイルの内容を文字列として受け取る
+  markdownContent: string;
 }
 
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ markdownContent }) => {
-  // Prism.jsのハイライトを適用
-  useEffect(() => {
-    Prism.highlightAllUnder(document);
-  }, []);
-
   return (
     <Markdown
-      remarkPlugins={[remarkMath]} // remark-mathで数式を解析
-      rehypePlugins={[[rehypeKatex]]}
+      remarkPlugins={[remarkMath]}
+      rehypePlugins={[rehypeKatex, rehypeHighlight]}
       components={{
         img: ({ node, ...props }) => (
           <span className="flex flex-col items-center my-4">
             <img
               {...props}
-              className="max-w-full h-auto rounded-lg shadow-md" // 画像のスタイリングにTailwind CSSを使用
+              className="max-w-full h-auto rounded-lg shadow-md"
               alt={props.alt || ''}
             />
             {props.alt && (
-              <span className="text-sm text-gray-500 mt-2 italic">{props.alt}</span> // altテキストを画像の下に表示
+              <span className="text-sm text-gray-500 mt-2 italic">{props.alt}</span>
             )}
           </span>
         )
